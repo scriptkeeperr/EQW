0dcfa6ead7150bf4b38f9962f98c9468cc11d6b9
scriptkeeperr <33675607+scriptkeeperr@users.noreply.github.com>
2022-06-06 15:08:15 -0700

    got messy, will clean

--- EQW/EQW.Designer.cs
+++ EQW/EQW.Designer.cs
@@ -29,36 +29,40 @@ namespace EQW {
             this.checkBoxAlt = new System.Windows.Forms.CheckBox();
             this.checkBoxCtrl = new System.Windows.Forms.CheckBox();
             this.checkBoxShift = new System.Windows.Forms.CheckBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
             this.buttonSave = new System.Windows.Forms.Button();
             this.buttonSetHotKey = new System.Windows.Forms.Button();
             this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.textBoxProcName = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // labelAddProfile
             // 
             this.labelAddProfile.AutoSize = true;
-            this.labelAddProfile.Font = new System.Drawing.Font("PMingLiU-ExtB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.labelAddProfile.Location = new System.Drawing.Point(12, 225);
+            this.labelAddProfile.Font = new System.Drawing.Font("PMingLiU-ExtB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelAddProfile.Location = new System.Drawing.Point(322, 10);
             this.labelAddProfile.Name = "labelAddProfile";
-            this.labelAddProfile.Size = new System.Drawing.Size(103, 21);
+            this.labelAddProfile.Size = new System.Drawing.Size(79, 16);
             this.labelAddProfile.TabIndex = 0;
             this.labelAddProfile.Text = "Add Profile";
             // 
             // textBoxProfileName
             // 
-            this.textBoxProfileName.Location = new System.Drawing.Point(121, 224);
+            this.textBoxProfileName.Location = new System.Drawing.Point(407, 8);
             this.textBoxProfileName.MaxLength = 100;
             this.textBoxProfileName.Name = "textBoxProfileName";
             this.textBoxProfileName.PlaceholderText = "name";
-            this.textBoxProfileName.Size = new System.Drawing.Size(165, 23);
+            this.textBoxProfileName.Size = new System.Drawing.Size(123, 23);
             this.textBoxProfileName.TabIndex = 1;
             // 
             // checkBoxAlt
             // 
             this.checkBoxAlt.AutoSize = true;
-            this.checkBoxAlt.Location = new System.Drawing.Point(384, 227);
+            this.checkBoxAlt.Location = new System.Drawing.Point(322, 41);
             this.checkBoxAlt.Name = "checkBoxAlt";
             this.checkBoxAlt.Size = new System.Drawing.Size(41, 19);
             this.checkBoxAlt.TabIndex = 3;
@@ -68,7 +72,7 @@ namespace EQW {
             // checkBoxCtrl
             // 
             this.checkBoxCtrl.AutoSize = true;
-            this.checkBoxCtrl.Location = new System.Drawing.Point(431, 227);
+            this.checkBoxCtrl.Location = new System.Drawing.Point(369, 41);
             this.checkBoxCtrl.Name = "checkBoxCtrl";
             this.checkBoxCtrl.Size = new System.Drawing.Size(45, 19);
             this.checkBoxCtrl.TabIndex = 4;
@@ -78,29 +82,29 @@ namespace EQW {
             // checkBoxShift
             // 
             this.checkBoxShift.AutoSize = true;
-            this.checkBoxShift.Location = new System.Drawing.Point(482, 227);
+            this.checkBoxShift.Location = new System.Drawing.Point(420, 41);
             this.checkBoxShift.Name = "checkBoxShift";
             this.checkBoxShift.Size = new System.Drawing.Size(50, 19);
             this.checkBoxShift.TabIndex = 5;
             this.checkBoxShift.Text = "Shift";
             this.checkBoxShift.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // dataGridView
             // 
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 257);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(707, 181);
-            this.dataGridView1.TabIndex = 7;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(322, 70);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 25;
+            this.dataGridView.Size = new System.Drawing.Size(286, 359);
+            this.dataGridView.TabIndex = 7;
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(644, 223);
+            this.buttonSave.Location = new System.Drawing.Point(572, 41);
             this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(75, 28);
+            this.buttonSave.Size = new System.Drawing.Size(40, 23);
             this.buttonSave.TabIndex = 8;
             this.buttonSave.Text = "Save";
             this.buttonSave.UseVisualStyleBackColor = true;
@@ -108,10 +112,9 @@ namespace EQW {
             // 
             // buttonSetHotKey
             // 
-            this.buttonSetHotKey.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.buttonSetHotKey.Location = new System.Drawing.Point(292, 223);
+            this.buttonSetHotKey.Location = new System.Drawing.Point(536, 7);
             this.buttonSetHotKey.Name = "buttonSetHotKey";
-            this.buttonSetHotKey.Size = new System.Drawing.Size(86, 28);
+            this.buttonSetHotKey.Size = new System.Drawing.Size(76, 23);
             this.buttonSetHotKey.TabIndex = 2;
             this.buttonSetHotKey.Text = "Set Hot Key";
             this.buttonSetHotKey.UseVisualStyleBackColor = true;
@@ -119,24 +122,56 @@ namespace EQW {
             // 
             // textBoxKey
             // 
-            this.textBoxKey.Location = new System.Drawing.Point(538, 225);
+            this.textBoxKey.Location = new System.Drawing.Point(476, 39);
             this.textBoxKey.Name = "textBoxKey";
             this.textBoxKey.PlaceholderText = "key";
-            this.textBoxKey.Size = new System.Drawing.Size(100, 23);
+            this.textBoxKey.Size = new System.Drawing.Size(90, 23);
             this.textBoxKey.TabIndex = 9;
             this.textBoxKey.Enter += new System.EventHandler(this.ButtonSetHotKey_Click);
             this.textBoxKey.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxKey_KeyDown);
             this.textBoxKey.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TextBoxKey_KeyUp);
             this.textBoxKey.Leave += new System.EventHandler(this.TextBoxKey_Leave);
             // 
+            // buttonFind
+            // 
+            this.buttonFind.Location = new System.Drawing.Point(12, 12);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(75, 23);
+            this.buttonFind.TabIndex = 10;
+            this.buttonFind.Text = "Find";
+            this.buttonFind.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += new System.EventHandler(this.ButtonFind_Click);
+            // 
+            // textBoxProcName
+            // 
+            this.textBoxProcName.Location = new System.Drawing.Point(90, 12);
+            this.textBoxProcName.Name = "textBoxProcName";
+            this.textBoxProcName.Size = new System.Drawing.Size(123, 23);
+            this.textBoxProcName.TabIndex = 11;
+            this.textBoxProcName.Text = "notepad";
+            this.textBoxProcName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxProcName_KeyDown);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(303, 388);
+            this.dataGridView1.TabIndex = 13;
+            // 
             // EQW
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(731, 450);
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBoxProcName);
+            this.Controls.Add(this.buttonFind);
             this.Controls.Add(this.textBoxKey);
             this.Controls.Add(this.buttonSave);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.checkBoxShift);
             this.Controls.Add(this.checkBoxCtrl);
             this.Controls.Add(this.checkBoxAlt);
@@ -145,6 +180,7 @@ namespace EQW {
             this.Controls.Add(this.labelAddProfile);
             this.Name = "EQW";
             this.Text = "EQW";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -158,10 +194,13 @@ namespace EQW {
         private System.Windows.Forms.CheckBox checkBoxAlt;
         private System.Windows.Forms.CheckBox checkBoxCtrl;
         private System.Windows.Forms.CheckBox checkBoxShift;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView;
         private System.Windows.Forms.Button buttonSave;
         private System.Windows.Forms.Button buttonSetHotKey;
         private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.TextBox textBoxProcName;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- EQW/EQW.cs
+++ EQW/EQW.cs
@@ -2,8 +2,11 @@
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
-//using System.Text.Json;
-//using System.Text.Json.Serialization;
+using System.Drawing;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.IO;
 
 namespace EQW {
 
@@ -12,56 +15,71 @@ namespace EQW {
         [DllImport("User32.dll", SetLastError = true)]
         static extern void SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
 
+        //public class Profile {
+        //    public string Name { get; set; }
+        //    public HotKey HotKey { get; set; }
 
-        public class Profile {
-            public string Name { get; set; }
-            public HotKey HotKey { get; set; }
-
-        }
+        //    public Profile(string name, HotKey hotKey) {
+        //        Name = name;
+        //        HotKey = hotKey;
+        //    }
+        //    public Profile() { }
+        //}
 
         #region test data
-        readonly string proccessName = "eqgame";
-        readonly Profile[] profiles = new Profile[] {
-                new Profile {Name = "Veeno",
-                    HotKey = new HotKey(Modifiers.CTRL, (uint)Keys.D1)},
-                new Profile {Name = "Krizma",
-                    HotKey = new HotKey(Modifiers.CTRL, (uint)Keys.D2)},
-                new Profile {Name = "Xlag",
-                    HotKey = new HotKey(Modifiers.CTRL, (uint)Keys.D3)}
-        };
+        string proccessName = "eqgame";
+
+        static readonly Dictionary<string, HotKey> profiles = new Dictionary<string, HotKey>();//{
+        //        new Profile {Name = "Veeno",
+        //            HotKey = new HotKey(Modifiers.ALT, (uint)Keys.D1)},
+        //        new Profile {Name = "Krizma",
+        //            HotKey = new HotKey(Modifiers.ALT, (uint)Keys.D2)},
+        //        new Profile {Name = "Xlag",
+        //            HotKey = new HotKey(Modifiers.ALT, (uint)Keys.D3)}
+        //};
         #endregion
 
-        bool labelAdded = false;
+
+        Control label;
+        Font icons = new Font("Segoe MDL2 Assets", 11);
 
         void InitializeProcessButtons() {
+            proccessName = textBoxProcName.Text;
             Process[] processes = Process.GetProcessesByName(proccessName);
 
             if (processes.Length > 0) {
                 AddControls(processes);
+                if (label != null) {
+                    label.Dispose();
+                }
                 return;
             }
+            AddLabel();
+        }
 
-            if (!labelAdded) {
-                var label = new Label() {
-                    Text = $"no {proccessName} running",
-                    Width = Width,
-                    AutoSize = true,
-                    Font = new System.Drawing.Font(
-                        System.Drawing.FontFamily.GenericMonospace.Name, 40),
-                    BorderStyle = BorderStyle.FixedSingle
+        void AddLabel() {
+            if (label == null) {
+                label = new Label() {
+                    Text = $"no {proccessName} running - click here after opening",
+                    Top = buttonFind.Bottom + 12,
+                    Left = 12,
                 };
+
+                label.Click += (o, e) => {
+                    InitializeProcessButtons();
+                };
+
                 Controls.Add(label);
-                labelAdded = true;
             }
         }
 
-        void AddControls(Process[] processes) {
-            int nextTop = 4;
+        void originalLayoutMethod(Process[] processes) {
+            int top = 12, left = 12;
             foreach (var p in processes) {
                 var b = new Button() {
                     Text = p.Id.ToString(),
-                    Top = nextTop,
-                    Left = 4,
+                    Top = top,
+                    Left = left,
                 };
 
                 b.Click += (o, e) => {
@@ -70,18 +88,18 @@ namespace EQW {
                 };
 
                 var cb = new ComboBox() {
-                    Top = nextTop,
+                    Top = top,
                     Left = b.Right + 4,
                     DropDownStyle = ComboBoxStyle.DropDownList
                 };
 
                 foreach (var pf in profiles) {
-                    cb.Items.Add(pf.Name);
+                    cb.Items.Add(pf.Key);
                 }
 
                 cb.SelectionChangeCommitted += (o, e) => {
-                    var profileHotKey = profiles[cb.SelectedIndex].HotKey;
-                    b.Text = profiles[cb.SelectedIndex].Name;
+                    var profileHotKey = profiles[cb.Text];
+                    b.Text = cb.Text;
                     var ghk = new GlobalHotKey(
                         Handle, p.MainWindowHandle,
                         profileHotKey.Modifiers,
@@ -89,11 +107,128 @@ namespace EQW {
                     );
                 };
 
-                Controls.Add(b);
-                Controls.Add(cb);
+                var del = new Button() {
+                    Top = top - 1,
+                    Left = cb.Right + 4,
+                    Font = icons,
+                    Text = "\uE74D",
+                    AutoSize = true,
+                    AutoSizeMode = AutoSizeMode.GrowAndShrink,
+                    Height = cb.Height,
+                };
 
-                nextTop = b.Bottom + 4;
+                del.Click += (o, e) => {
+                    top = b.Top;
+                    b.Dispose();
+                    cb.Dispose();
+                    del.Dispose();
+                    buttonFind.Top = textBoxProcName.Top = top;
+                };
+
+                top = b.Bottom + 4;
             }
+        }
+        static DataGridViewComboBoxCell ProfilesBox;
+        void AddControls(Process[] processes) {
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.Font = icons;
+            if (ProfilesBox == null) {
+                ProfilesBox = new DataGridViewComboBoxCell();
+            } else {
+                ProfilesBox.Items.Clear();
+            }
+            ProfilesBox.Items.AddRange(profiles);
+
+            var bcWindow = new DataGridViewButtonColumn() {
+                Name = "Window",
+                DataPropertyName = "PID",
+                ReadOnly = true,
+            };
+
+            var cbcProfiles = new DataGridViewComboBoxColumn() {
+                Name = "Profiles",
+                DataPropertyName = "Profiles",
+            };
+
+            var bcDelete = new DataGridViewButtonColumn() {
+                Name = "Delete",
+                Text = "\uE74E",
+            };
+            bcDelete.Text = "shit";
+
+            dataGridView1.Columns.AddRange(
+                new DataGridViewColumn[] { bcWindow, cbcProfiles, bcDelete }
+            );
+
+            foreach (var pf in profiles) {
+                cbcProfiles.Items.Add(pf.Key);
+            }
+
+            DataGridViewRow row = null;
+            foreach (var p in processes) {
+                row = new DataGridViewRow();
+                row.Cells.Add(new DataGridViewButtonCell() {
+                    Value = p.Id.ToString(),
+                    Tag = p.MainWindowHandle
+                });
+                row.Cells.Add(ProfilesBox);
+                row.Cells.Add(new DataGridViewButtonCell() {
+                    Value = "\uE74E",
+                    Tag = p.MainWindowHandle
+                });
+
+                dataGridView1.Rows.Add(row);
+            }
+            row = null;
+
+            dataGridView1.CellClick += (o, e) => {
+                if (e.RowIndex >= 0) {
+                    if (e.ColumnIndex == dataGridView1.Columns["Window"].Index) {
+                        var handle =
+                            (IntPtr)dataGridView1[e.ColumnIndex, e.RowIndex].Tag;
+                        SwitchToThisWindow(handle, true);
+                    }
+                }
+            };
+
+            dataGridView1.CellValueChanged += (o, e) => {
+                var profileHotKey = profiles[
+                    dataGridView1[e.ColumnIndex, e.RowIndex].Value.ToString()
+                ];
+
+                var ghk = new GlobalHotKey(
+                    Handle,
+                    (IntPtr)dataGridView1[
+                        dataGridView1.Columns["Window"].Index, e.RowIndex
+                    ].Tag,
+                    profileHotKey.Modifiers,
+                    profileHotKey.Key
+                );
+
+                dataGridView1.Invalidate();
+            };
+
+            dataGridView1.CurrentCellDirtyStateChanged += (o, e) => {
+                if (dataGridView1.IsCurrentCellDirty) {
+                    dataGridView1.CommitEdit(DataGridViewDataErrorContexts.Commit);
+                }
+            };
+
+            dataGridView1.MultiSelect = false;
+
+            dataGridView1.SelectionChanged += (o, e) => {
+                var cel = o as DataGridViewComboBoxCell;
+                if (cel != null) {
+                    var profileHotKey = (HotKey)cel.Value;//profiles[cb.Text];
+                    //b.Text = cb.Text;
+                    var ghk = new GlobalHotKey(
+                        Handle, //p.MainWindowHandle,
+                        (IntPtr)dataGridView1.SelectedRows[0].Cells["Window"].Tag,
+                        profileHotKey.Modifiers,
+                        profileHotKey.Key
+                    ); ;
+                }
+            };
         }
 
         protected override void WndProc(ref Message m) {
@@ -109,11 +244,19 @@ namespace EQW {
 
         public EQW() {
             InitializeComponent();
+            profiles.Add("Veeno", new HotKey(Modifiers.ALT, (uint)Keys.D1));
+            profiles.Add("Krizma", new HotKey(Modifiers.ALT, (uint)Keys.D2));
+            profiles.Add("Xlag", new HotKey(Modifiers.ALT, (uint)Keys.D3));
+            dataGridView1.Font = icons;
+            buttonFind.Font = buttonSave.Font = icons;
+            buttonFind.Text = "\uE773";
+            buttonSave.Text = "\uE74E";
             InitializeProcessButtons();
             FormClosing += EQW_FormClosing;
-            dataGridView1.DataSource = profiles;
+            dataGridView.DataSource = profiles;
         }
 
+        #region event functions
         private void ButtonSetHotKey_Click(object sender, EventArgs e) {
             buttonSetHotKey.Text = "Listening";
             if (!textBoxKey.Focused) {
@@ -126,6 +269,7 @@ namespace EQW {
             CheckBoxes(e);
             if ((e.KeyCode & (Keys.ControlKey | Keys.Menu | Keys.ShiftKey)) != e.KeyCode) {
                 textBoxKey.Text = Enum.GetName(typeof(Keys), e.KeyCode);
+                textBoxKey.Tag = (uint)e.KeyCode;
                 buttonSave.Focus();
             }
         }
@@ -147,22 +291,50 @@ namespace EQW {
         }
 
         private void ButtonSave_Click(object sender, EventArgs e) {
-            //var mods = Modifiers.NONE;
+            var mods = Modifiers.NONE;
 
-            //mods |= checkBoxAlt.Checked ? Modifiers.ALT : 0;
-            //mods |= checkBoxCtrl.Checked ? Modifiers.CTRL : 0;
-            //mods |= checkBoxShift.Checked ? Modifiers.SHIFT : 0;
+            mods |= checkBoxAlt.Checked ? Modifiers.ALT : 0;
+            mods |= checkBoxCtrl.Checked ? Modifiers.CTRL : 0;
+            mods |= checkBoxShift.Checked ? Modifiers.SHIFT : 0;
 
             // get the key
+            var key = (uint)textBoxKey.Tag;
 
             // get the name
+            var name = textBoxProfileName.Text;
 
+            //var profile = new Profile(name, new HotKey(mods, key));
             // save to disk
+
+            //dataGridView.Rows.Add(profile);
+            //await Save();
+            //profiles.Add(profile);
+            profiles.Add(name, new HotKey(mods, key));
+            Save();
+            dataGridView.Refresh();
+        }
+
+        static void Save() {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(profiles, options);
+            string fileName = "profiles.json";
+            File.WriteAllText(fileName, json);
         }
 
         private void EQW_FormClosing(object sender, FormClosingEventArgs e) {
             GlobalHotKey.Handles.Clear();
         }
+
+        private void ButtonFind_Click(object sender, EventArgs e) {
+            InitializeProcessButtons();
+        }
+
+        private void TextBoxProcName_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+                InitializeProcessButtons();
+            }
+        }
+        #endregion
 
     }
 }
